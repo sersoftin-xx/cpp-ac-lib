d51d06f7fb9b912430b6d481ea6b5eb7e58bf074
Sergo <sersoftin@gmail.com>
2016-05-30 18:08:26 +0300

    Реализация подсчета sha256 из OpenSSL

--- CppACLib/Util.cpp
+++ CppACLib/Util.cpp
@@ -1,2 +1,22 @@
 #include "Util.h"
 
+std::string Util::sha256(std::string input_string)
+{
+	unsigned char hash[SHA256_DIGEST_LENGTH];
+	SHA256_CTX sha256;
+	SHA256_Init(&sha256);
+	SHA256_Update(&sha256, input_string.c_str(), input_string.size());
+	SHA256_Final(hash, &sha256);
+	return toHex(hash);
+}
+
+std::string Util::toHex(unsigned char* hash)
+{
+	char result[SHA256_DIGEST_LENGTH * 2 + 1];
+	for (auto i = 0; i < SHA256_DIGEST_LENGTH; ++i)
+	{
+		sprintf(result + i * 2, "%02X", hash[i]);
+	}
+	result[SHA256_DIGEST_LENGTH * 2] = 0x00;
+	return std::string(result);
+}
--- CppACLib/Util.h
+++ CppACLib/Util.h
@@ -1,9 +1,12 @@
 #pragma once
 #include <string>
+#include <openssl/sha.h>
 
 class Util
 {
 public:
 	static std::string sha256(std::string input_string);
+private:
+	static std::string toHex(unsigned char* hash);
 };
 
