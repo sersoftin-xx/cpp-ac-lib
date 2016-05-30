59d6cd3c3e3fe2d8140c70f18008044e586da2bc
Sergo <sersoftin@gmail.com>
2016-05-30 15:45:24 +0300

    Обернул AccessRequest в неймспейс AccessControlLibrary::Entities

--- CppACLib/AccessRequest.cpp
+++ CppACLib/AccessRequest.cpp
@@ -1,40 +1,45 @@
 #include "AccessRequest.h"
 
-
-AccessRequest::AccessRequest(const std::string& pc_name, const std::string& pc_unique_key, int product_id)
-	: _pc_name(pc_name),
-	_pc_unique_key(pc_unique_key),
-	_product_id(product_id)
+namespace AccessControlLibrary
 {
+	namespace Entities
+	{
+		AccessRequest::AccessRequest(const std::string& pc_name, const std::string& pc_unique_key, int product_id)
+			: _pc_name(pc_name),
+			_pc_unique_key(pc_unique_key),
+			_product_id(product_id)
+		{
 
-}
+		}
 
-std::string AccessRequest::getPcName() const
-{
-	return _pc_name;
-}
+		std::string AccessRequest::getPcName() const
+		{
+			return _pc_name;
+		}
 
-std::string AccessRequest::getPcUniqueKey() const
-{
-	return _pc_unique_key;
-}
+		std::string AccessRequest::getPcUniqueKey() const
+		{
+			return _pc_unique_key;
+		}
 
-int AccessRequest::getProductId() const
-{
-	return _product_id;
-}
+		int AccessRequest::getProductId() const
+		{
+			return _product_id;
+		}
 
-void AccessRequest::setPcName(std::string pc_name)
-{
-	_pc_name = pc_name;
-}
+		void AccessRequest::setPcName(std::string pc_name)
+		{
+			_pc_name = pc_name;
+		}
 
-void AccessRequest::setPcUniqueKey(std::string pc_unique_key)
-{
-	_pc_unique_key = pc_unique_key;
-}
+		void AccessRequest::setPcUniqueKey(std::string pc_unique_key)
+		{
+			_pc_unique_key = pc_unique_key;
+		}
 
-void AccessRequest::setProductId(int product_id)
-{
-	_product_id = product_id;
+		void AccessRequest::setProductId(int product_id)
+		{
+			_product_id = product_id;
+		}
+	}
 }
--- CppACLib/AccessRequest.h
+++ CppACLib/AccessRequest.h
@@ -1,21 +1,27 @@
 #pragma once
 #include <string>
 
-class AccessRequest
+namespace AccessControlLibrary
 {
-public:
-	AccessRequest(const std::string& pc_name, const std::string& pc_unique_key, int product_id);
+	namespace Entities
+	{
+		class AccessRequest
+		{
+		public:
+			AccessRequest(const std::string& pc_name, const std::string& pc_unique_key, int product_id);
 
-	std::string getPcName() const;
-	std::string getPcUniqueKey() const;
-	int getProductId() const;
+			std::string getPcName() const;
+			std::string getPcUniqueKey() const;
+			int getProductId() const;
 
-	void setPcName(std::string pc_name);
-	void setPcUniqueKey(std::string pc_unique_key);
-	void setProductId(int product_id);
-private:
-	std::string _pc_name;
-	std::string _pc_unique_key;
-	int _product_id;
-};
+			void setPcName(std::string pc_name);
+			void setPcUniqueKey(std::string pc_unique_key);
+			void setProductId(int product_id);
 
+		private:
+			std::string _pc_name;
+			std::string _pc_unique_key;
+			int _product_id;
+		};
+	}
+}
