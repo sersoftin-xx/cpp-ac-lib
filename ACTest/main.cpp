51c454197bd664ccd67d30376c9a5cbe3702630b
Sergey Sergeev <sersoftin@gmail.com>
2016-06-03 15:14:33 +0300

    Включил обязательную проверку сертификата по хешу публичного ключа (sha1)

--- ACTest/main.cpp
+++ ACTest/main.cpp
@@ -12,7 +12,7 @@ int main()
 	config->setBaseApiUrl("https://acs.sersoftin.xyz/");
 	config->setHashSalt("TestHash");
 	config->setProductId(1);
-	config->setCheckCertificate(false);
+	config->setCheckCertificate(true);
 	config->setPublicKeyHash({ 0x9f, 0x71, 0x41, 0x48, 0x67, 0x18, 0x73, 0xce, 0x77, 0x1b, 0xe6, 0x14, 0x0c, 0x25, 0x22, 0xb1, 0x4d, 0xda, 0xf5, 0x12 });
 	auto acl = new AccessControlLibrary::AccessControl(config);
 
