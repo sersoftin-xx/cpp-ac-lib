4a0c14319db8b41b78b6aa84ba2c0169e14df14c
Sergey Sergeev <sersoftin@gmail.com>
2016-06-03 15:12:47 +0300

    Добавлена тестовая программа для теста привязки Полностью доделана программа для подачи заявки Настроены конфигурации, чтобы в статическую библиотеку не попадали другие статические файлы (openssl, jsoncpp, libcurl)

---
+++ ACTest/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "../CppACLib/AccessControl.h"
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	SetConsoleTitleW(L"AC Test Application");
+	std::cout << "AC Test - программа, которая позволяет проверить доступ ПО." << std::endl;
+
+	auto config = new AccessControlLibrary::Configuration();
+	config->setBaseApiUrl("https://acs.sersoftin.xyz/");
+	config->setHashSalt("TestHash");
+	config->setProductId(1);
+	config->setCheckCertificate(false);
+	config->setPublicKeyHash({ 0x9f, 0x71, 0x41, 0x48, 0x67, 0x18, 0x73, 0xce, 0x77, 0x1b, 0xe6, 0x14, 0x0c, 0x25, 0x22, 0xb1, 0x4d, 0xda, 0xf5, 0x12 });
+	auto acl = new AccessControlLibrary::AccessControl(config);
+
+	std::cout << (acl->accessAllowed() ? "Доступ разрешен!" : "Вы не имете права на использование этой программы.") << std::endl;
+	delete config;
+	delete acl;
+	getchar();
+	return 0;
+}
--- ACTestApplication/main.cpp
+++ ACTestApplication/main.cpp
@@ -4,42 +4,38 @@
 
 int main()
 {
+	setlocale(LC_ALL, "rus");
+	SetConsoleTitleW(L"AC Test Application");
+	std::cout << "ACTestApplication — приложение, которое позволяет продемонстрировать возможности библиотеки AccessControl." << std::endl;
+
+	auto config = new AccessControlLibrary::Configuration();
+	config->setBaseApiUrl("https://acs.sersoftin.xyz/");
+	config->setHashSalt("TestHash");
+	config->setProductId(1);
+	config->setCheckCertificate(false);
+	config->setPublicKeyHash({ 0x9f, 0x71, 0x41, 0x48, 0x67, 0x18, 0x73, 0xce, 0x77, 0x1b, 0xe6, 0x14, 0x0c, 0x25, 0x22, 0xb1, 0x4d, 0xda, 0xf5, 0x12 });
+	auto acl = new AccessControlLibrary::AccessControl(config);
+	std::cout << "Список продуктов:" << std::endl;
+	for (auto & product: acl->getProducts())
+	{
+		std::cout << product.getId() << ":" << product.getName() << std::endl;
+	}
+	auto request_id = 0;
+	std::cout << "Введите номер продукта, на который вы хотите подать заявку: ";
+	std::cin >> request_id;
+
 	try
 	{
-		setlocale(LC_ALL, "rus");
-		SetConsoleTitleW(L"ACTestApplication");
-		std::cout << "ACTestApplication — приложение, которое позволяет продемонстрировать возможности библиотеки AccessControl." << std::endl;
-		std::cout << "Список продуктов:" << std::endl;
-		AccessControlLibrary::Configuration * config = new AccessControlLibrary::Configuration();
-		config->setBaseApiUrl("http://acs.sersoftin.xyz/");
-		config->setHashSalt("TestHash");
-		config->setProductId(1);
-		config->setCheckCertificate(false);
-		config->setPublicKeyHash({ 0x9f, 0x71, 0x41, 0x48, 0x67, 0x18, 0x73, 0xce, 0x77, 0x1b, 0xe6, 0x14, 0x0c, 0x25, 0x22, 0xb1, 0x4d, 0xda, 0xf5, 0x12 });
-		AccessControlLibrary::AccessControl * acl = new AccessControlLibrary::AccessControl(config);
-		
-		auto request_id = 0;
-		std::cout << "Введите номер продукта, на который вы хотите подать заявку: ";
-		std::cin >> request_id;
-
-		try
-		{
-			auto bid = acl->requestAccess(request_id);
-			std::cout << "bid id: " << bid.getId() << std::endl;
-		}
-		catch (AccessControlLibrary::Exceptions::WebException & ex)
-		{
-			std::cout << "WebException: " << ex.getError().getCode() << ":" << ex.getError().getMessage() << ":" << ex.getError().getUrl() << std::endl;
-		}
-		delete config;
-		delete acl;
-		getchar();
-		getchar();
+		auto bid = acl->requestAccess(request_id);
+		std::cout << "Номер вашей заявки: " << bid.getId() << std::endl;
 	}
-	catch (std::runtime_error ex)
+	catch (AccessControlLibrary::Exceptions::WebException & ex)
 	{
-		std::cout << ex.what() << std::endl;
-		getchar();
+		std::cout << "WebException: " << ex.getError().getCode() << ":" << ex.getError().getMessage() << ":" << ex.getError().getUrl() << std::endl;
 	}
+	delete config;
+	delete acl;
+	getchar();
+	getchar();
 	return 0;
 }
